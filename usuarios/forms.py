e005027479f287afac095401e25537359842eb37
Gabriel Galani <109910042+GabrielGalani@users.noreply.github.com>
2024-03-30 16:32:34 -0300

    Add: Clean validation on password and username

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -26,50 +26,71 @@ class LoginFroms(forms.Form):
 
   
 class CadastroFroms(forms.Form):
-        nome_cadastro=forms.CharField(
-            label="Nome de login",
-            required=True,
-            max_length=100,
-            widget=forms.TextInput(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Ex.: Gabriel Galani"
-                }
-            )
+    nome_cadastro=forms.CharField(
+        label="Nome de login",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: Gabriel Galani"
+            }
         )
+    )
+    
+    email=forms.EmailField(
+        label="Email",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: gabrielgalani@xpto.com"
+            }
+        )
+    )
+    
+    senha_um= forms.CharField(
+        label = "Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
+    
+    senha_dois= forms.CharField(
+        label = "Confirme sua senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha novamente"
+            }
+        )
+    )
+    
+    def clean_nome_cadastro(self):
+        nome=self.cleaned_data.get("nome_cadastro")
         
-        email=forms.EmailField(
-            label="Email",
-            required=True,
-            max_length=100,
-            widget=forms.EmailInput(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Ex.: gabrielgalani@xpto.com"
-                }
-            )
-        )
+        if nome:
+            nome = nome.strip()
+            if " " in nome:
+                raise forms.ValidationError("Espaços não são permitidos no campo de nome de usuário")
+            else:
+                return nome
+
+    
+    def clean_senha_dois(self):
+        senha_um=self.cleaned_data.get("senha_um")
+        senha_dois=self.cleaned_data.get("senha_dois")
         
-        senha_um= forms.CharField(
-            label = "Senha",
-            required=True,
-            max_length=70,
-            widget=forms.PasswordInput(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Digite sua senha"
-                }
-            )
-        )
-        
-        senha_dois= forms.CharField(
-            label = "Confirme sua senha",
-            required=True,
-            max_length=70,
-            widget=forms.PasswordInput(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Digite sua senha novamente"
-                }
-            )
-        )
+        if senha_um and senha_dois: 
+            if senha_um != senha_dois: 
+                raise forms.ValidationError("As senhas devem ser iguais!")
+            else: 
+                return senha_dois
